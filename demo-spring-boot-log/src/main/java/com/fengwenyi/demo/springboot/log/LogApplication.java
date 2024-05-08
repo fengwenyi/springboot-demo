b8e7ba826d5cbb0efee546c47703e1a5d03a6eaa
ErwinFeng <xfsy_2015@163.com>
2024-05-08 18:17:45 +0800

    logback

---
+++ demo-spring-boot-log/src/main/java/com/fengwenyi/demo/springboot/log/LogApplication.java
@@ -0,0 +1,31 @@
+package com.fengwenyi.demo.springboot.log;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author <a href="https://fengwenyi.com">Erwin Feng</a>
+ * @since 2024-05-08
+ */
+@RestController
+@SpringBootApplication
+public class LogApplication {
+
+    private static final Log log = LogFactory.getLog(LogApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        log.info("home() is called");
+        return "Hello World!";
+    }
+
+}
