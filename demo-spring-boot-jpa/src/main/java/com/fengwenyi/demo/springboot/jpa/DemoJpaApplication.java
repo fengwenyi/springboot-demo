d7a18cc068436ab4a0ee6f7cc4ec922902c20eb1
ErwinFeng <xfsy_2015@163.com>
2024-11-05 01:03:49 +0800

    改成 maven 工程

---
+++ demo-spring-boot-jpa/src/main/java/com/fengwenyi/demo/springboot/jpa/DemoJpaApplication.java
@@ -0,0 +1,17 @@
+package com.fengwenyi.demo.springboot.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="https://fengwenyi.com">Erwin Feng</a>
+ * @since 2024-11-05
+ */
+@SpringBootApplication
+public class DemoJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJpaApplication.class, args);
+    }
+
+}
